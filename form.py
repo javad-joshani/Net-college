662e755dcf63ffb6354dbb06b4d3061748eab2ef
javad-joshani <53216859+javad-joshani@users.noreply.github.com>
2024-08-07 19:43:43 +0330

    form.py

--- form.py
+++ form.py
@@ -36,6 +36,66 @@ while True:
     if choise == "s":
         print(dict)
 
+# ---------------------------------- NEXT -----------------------------------------
+
+    from tkinter import messagebox
+dict = {}
+def crate(people):
+    name = input("name:")
+    famil = input("famil:")
+    age = input("age:")
+
+    person_id = len(people) + 1
+    people[person_id] ={
+        "name" : name,
+        "famil" : famil,
+        "age" : age
+    }
+    messagebox.showinfo("see mee",f"{name} {famil} age: {age} crated.")
+    
+
+def remove(people):
+    person_id = int(input("remove id :"))
+    if person_id in people:
+        del people[person_id]
+        print("removed !!!")
+    else:
+        print("not founded person")
+
+def update(people):
+    person_id = int(input("update id :"))
+
+    if person_id in people:
+        name = input("name:")
+        famil = input("famil:")
+        age = input("age:")
+
+        people[person_id] = {
+            "name" : name,
+            "famil" : famil,
+            "age" : age
+        }
+        print("person updated !!!")
+    else:
+        print("person not fonded !!!")
+
+while True:
+    print("// c // for crate")
+    print("// u // for update")
+    print("// r // for remove")
+    print("// q // for quite")
+
+    choise = input("what do you want??")
+    if choise == "c":
+        crate(dict)
+    elif choise == "u":
+        update(dict)
+    elif choise == "r":
+        remove(dict)
+    elif choise == "q":
+        print("out")
+        break
+
     if choise == "q":
         print("finish")
         break
